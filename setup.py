6773295582dffd4c4ea17b5061299e50b66406c3
Franck L. <78041712+claaudelane@users.noreply.github.com>
2025-04-24 17:03:42 +0200

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xml-patcher",
+    version="1.0.0",
+    description="A tool for patching and modifying XML files with YAML configuration",
+    author="Franck L.",
+    author_email="fcl@aubonrepas.com",
+    packages=find_packages(include=['xml_patcher', 'xml_patcher.*']),
+    package_dir={'': 'src'},
+    package_data={
+        'xml_patcher': ['templates/*', 'config/*'],
+    },
+    install_requires=[
+        "pyyaml>=6.0",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "xml-patcher=xml_patcher.patcher:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+) 
